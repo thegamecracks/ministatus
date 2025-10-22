1ba831d922b32e508247b0b34a70028950aecb36
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2025-10-22 06:19:44 -0400

    feat: replace CLI with click

--- src/ministatus/cli/__main__.py
+++ src/ministatus/cli/__main__.py
@@ -1,37 +1,18 @@
 import logging
-import sys
 
-from ministatus.cli.config import Config
-from ministatus.cli.errors import CommandError
+import click
+
+from ministatus import __version__
 from ministatus.logging import setup_logging
 
 log = logging.getLogger(__name__)
 
 
-def main() -> None:
-    try:
-        config = Config.parse_args()
-        setup_logging(verbose=config.verbose)
-        try_invoke(config)
-    except EOFError:
-        print()
-        sys.exit(1)
-    except KeyboardInterrupt:
-        print()
-        sys.exit(130)
-    except Exception as e:
-        log.exception(e)
-        sys.exit(1)
-
-def try_invoke(config: Config) -> None:
-    try:
-        config.invoke()
-    except CommandError as e:
-        if config.verbose:
-            log.exception(e)
-        else:
-            log.error("%s\n(To show full traceback, use -v/--verbose)", e)
-        sys.exit(1)
+@click.group()
+@click.option("-v", "--verbose", count=True)
+@click.version_option(__version__, "-V", "--version")
+def main(verbose: int) -> None:
+    setup_logging(verbose=verbose)
 
 
 if __name__ == "__main__":
--- src/ministatus/cli/errors.py
+++
@@ -1,2 +0,0 @@
-class CommandError(Exception):
-    """Raised when an error occurs during command execution."""
