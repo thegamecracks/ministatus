d05fafa0592d2adfb2a7d40542b3acbb9a5dce84
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2025-10-22 15:36:34 -0400

    feat: show db path in appdirs command

--- src/ministatus/cli/commands/appdirs.py
+++ src/ministatus/cli/commands/appdirs.py
@@ -1,10 +1,11 @@
 import click
 
-from ministatus.appdirs import APP_DIRS
+from ministatus.appdirs import APP_DIRS, DB_PATH
 
 
 @click.command()
 def appdirs() -> None:
-    """Show directories used by this application."""
+    """Show directories and important files used by this application."""
     click.echo(f"user_data_path    = {APP_DIRS.user_data_path}")
     click.echo(f"user_log_path     = {APP_DIRS.user_log_path}")
+    click.echo(f"DB_PATH           = {DB_PATH}")
