00d2604f9ec18b1409e8e5a1acee9c347caaf7a6
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2025-10-23 21:17:22 -0400

    feat: remove unused status subcommands

--- src/ministatus/bot/cogs/status/cog.py
+++ src/ministatus/bot/cogs/status/cog.py
@@ -47,41 +47,6 @@ class StatusCog(
         view = StatusManageView(interaction, statuses)
         await view.send(interaction, ephemeral=True)
 
-    @app_commands.command(name="create")
-    async def status_create(
-        self,
-        interaction: discord.Interaction[Bot],
-        label: str,
-    ) -> None:
-        """Create a status entry.
-
-        :param label: The label to assign to your status.
-
-        """
-        assert interaction.guild is not None
-        assert isinstance(interaction.user, discord.Member)
-
-        status = Status(status_id=0, guild_id=interaction.guild.id, label=label)
-        async with connect_discord_database_client(self.bot) as ddc:
-            await ddc.add_member(interaction.user)
-            status = await ddc.client.create_status(status)
-
-        message = f'Created status "{status.label}"!'
-        await interaction.response.send_message(message, ephemeral=True)
-
-    alert = app_commands.Group(
-        name="alert",
-        description="Manage server status alerts.",
-    )
-    display = app_commands.Group(
-        name="display",
-        description="Manage server status displays.",
-    )
-    query = app_commands.Group(
-        name="query",
-        description="Manage server status query.",
-    )
-
     @tasks.loop(seconds=60)
     async def query_loop(self) -> None:
         guild_ids = [guild.id for guild in self.bot.guilds]
