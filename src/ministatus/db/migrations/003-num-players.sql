b65d1ad864720d500a788f7abdf000f706226833
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2025-10-28 11:04:30 -0400

    refactor!: ensure num players cannot exceed max players

--- src/ministatus/db/migrations/003-num-players.sql
+++ src/ministatus/db/migrations/003-num-players.sql
@@ -1,5 +1,5 @@
 ALTER TABLE status_history ADD COLUMN
-    num_players INTEGER NOT NULL DEFAULT 0 CHECK (num_players >= 0);
+    num_players INTEGER NOT NULL DEFAULT 0 CHECK (num_players >= 0 AND num_players <= max_players);
 ALTER TABLE status_display ADD COLUMN
     graph_interval INTERVAL NOT NULL DEFAULT 86400000 CHECK (graph_interval >= 0);
 
