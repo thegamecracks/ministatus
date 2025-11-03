6f62735bec14e36dfff4426beeaa5fca69b8de48
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2025-11-03 08:04:11 -0500

    feat: add styling to appdirs command

--- src/ministatus/cli/commands/appdirs.py
+++ src/ministatus/cli/commands/appdirs.py
@@ -6,5 +6,12 @@ from ministatus.appdirs import APP_DIRS
 @click.command()
 def appdirs() -> None:
     """Show directories and important files used by this application."""
-    click.echo(f"user_data_path    = {APP_DIRS.user_data_path}")
-    click.echo(f"user_log_path     = {APP_DIRS.user_log_path}")
+
+    def echo_appdirs_path(attr: str) -> None:
+        value = getattr(APP_DIRS, attr)
+        name = click.style(format(attr, "<17"), fg="yellow")
+        value = click.style(value, fg="green")
+        click.secho(f"{name} = {value}")
+
+    echo_appdirs_path("user_data_path")
+    echo_appdirs_path("user_log_path")
